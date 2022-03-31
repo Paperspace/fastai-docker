3567c06d78a4bc636d823191571591a5fc440b80
Chris Wetherill <89031899+cwetherill-ps@users.noreply.github.com>
2022-03-31 15:11:06 -0400

    chore(datasets): auto-mount fastai public dataset (#11)

--- fastai-fastbook/run.sh
+++ fastai-fastbook/run.sh
@@ -4,4 +4,11 @@ export SHELL=/bin/bash
 mkdir /storage/data
 rm -rf /storage/lost+found
 ln -s /datasets/fastai/* /storage/data/
-jupyter notebook --ip=0.0.0.0 --no-browser --allow-root --NotebookApp.trust_xheaders=True --NotebookApp.disable_check_xsrf=False --NotebookApp.allow_remote_access=True --NotebookApp.allow_origin='*'
+
+mkdir /notebooks/.gradient
+echo "integrations:
+  fastai:
+    type: dataset
+    ref: paperspace/dsr4qbcrcg662pf:latest" > /notebooks/.gradient/settings.yaml
+
+jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
