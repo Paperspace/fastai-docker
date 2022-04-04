0e4cc534a4a7763471a048f05f83981d780845a1
Chris Wetherill <89031899+cwetherill-ps@users.noreply.github.com>
2022-04-04 12:11:07 -0400

    chore(datasets): additional runtime compatibility with datasets (#12)
    We have historically used `/storage/data` as a symlink pointing to
    `/datasets`. This broke with the recent feature launch to allow dataset
    mounting within notebooks: the fact that this dependency on the
    now-replaced `/datasets` filepath existed in this runtime, and its
    fragility, were not documented, and this was not shared knowledge.
    
    This commit removed some lingering symlinks to now-nonexistent
    `/datasets/fastai` references which had been causing errors within the
    fastbook notebooks. It also reverts the `config.ini` update pointing the
    datasets filepath to `/datasets/fastai` directly.
    
    The fastai Python package requires the ability to write to whatever our
    datasets filepath is, primarily to create intermediary datasets (e.g.,
    as the result of work tokenization for NLP tasks). Since `/datasets` is
    a readonly mount, we need to persist the `/storage/data` indirection
    layer, so that we can continue to reference the automounted readonly
    automounted datasets while still allowing for system-writable paths to
    other intermediary datasets.

--- fastai-fastbook/run.sh
+++ fastai-fastbook/run.sh
@@ -5,6 +5,21 @@ mkdir /storage/data
 rm -rf /storage/lost+found
 ln -s /datasets/fastai/* /storage/data/
 
+rm -f /storage/data/aclImdb
+rm -f /storage/data/adult_sample
+rm -f /storage/data/camvid_tiny
+rm -f /storage/data/dogscats
+rm -f /storage/data/human_numbers
+rm -f /storage/data/imagenet-sample-train
+rm -f /storage/data/ml-100k
+rm -f /storage/data/mnist_sample
+rm -f /storage/data/movie_lens_sample
+rm -f /storage/data/trn_resized_288.bc
+rm -f /storage/data/trn_resized_72.bc
+rm -f /storage/data/wt103_ids
+
+rm -f /storage/data/imdb_tok
+
 mkdir /notebooks/.gradient
 echo "integrations:
   fastai:
