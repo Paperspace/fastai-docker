d98238ed115a3aea095af233bb81bf56d3c585a0
e-r-i-k-a <erika@paperspace.com>
2022-05-09 18:59:14 +0000

    update python, add packages

--- fastai-fastbook/run.sh
+++ fastai-fastbook/run.sh
@@ -26,4 +26,8 @@ echo "integrations:
     type: dataset
     ref: paperspace/dsr4qbcrcg662pf:latest" > /notebooks/.gradient/settings.yaml
 
+if [ -f /storage/pre-run.sh ]; then
+. /storage/pre-run.sh
+fi
+
 jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
