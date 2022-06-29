3611ebf929b035c72c7c34cade4610900401c8c3
joshua-paperspace <97624824+joshua-paperspace@users.noreply.github.com>
2022-06-29 14:04:15 -0700

    Update to include mambaforge and python3.9

--- fastai-fastbook/run.sh
+++ fastai-fastbook/run.sh
@@ -30,4 +30,4 @@ if [ -f /storage/pre-run.sh ]; then
 . /storage/pre-run.sh
 fi
 
-jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
+/root/mambaforge/bin/jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
